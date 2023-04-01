cf775ba296b3622349c47fc77979c32edbec1c2f
gagan <gaganrehal93@gmail.com>
2023-04-01 16:33:01 +0530

    login update

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -1,14 +1,16 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch,faBars } from "@fortawesome/free-solid-svg-icons";
 import {motion} from 'framer-motion';
 import '../../pages/HomeStyles/style/navbar.css'
-import { NavLink } from "react-router-dom";
+import { NavLink, json } from "react-router-dom";
 import navlogo from '../../images/NavLogo.jpeg'
+import {IoMdArrowDropdown} from 'react-icons/io'
+
+
 
 
 const NavMenu = [
-  { icon: "", title: "Blog", link: "" },
   { icon: "", title: "Home", link: "" },
   { icon: "", title: "Help", link: "" },
   { icon: "", title: "Login/Sign Up", link: "/signin" },
@@ -16,7 +18,8 @@ const NavMenu = [
 
 function NavBar() {
   const [isMobileNavOpen,setIsMobileNavOpen] = useState(false)
-
+  const [isOpenProfile,setIsOpenProfile] = useState(false)
+  const [isLogin,setIsLogin] = useState({status:false})
   const mobNavVariante = {
     isOpen :{
       opacity : 1,
@@ -27,6 +30,27 @@ function NavBar() {
       opacity : 0,
     }
   }
+  function handlelogout(){
+    if(localStorage.getItem('user') && localStorage.getItem('auth')){
+      localStorage.removeItem('user')
+      localStorage.removeItem('auth')
+      setIsLogin({
+        status:false
+      })
+    }
+  }
+  
+  useEffect(()=>{
+    if(localStorage.getItem('user') && localStorage.getItem('auth')){
+      let userdata = JSON.parse(localStorage.getItem('user'))
+      setIsLogin({
+        ...isLogin,
+        status:true,
+        ...userdata
+      })
+    }
+  },[])
+
   return (
     <nav className="com_nav">
       <motion.div 
@@ -48,11 +72,29 @@ function NavBar() {
 
           {NavMenu.map((menu, idx) => {
             return (
+              isLogin.status?
+                menu.title !== 'Login/Sign Up' &&
+              <NavLink to={menu.link} className="nav-links-menu" key={menu.title + idx}>
+                {menu.title}
+              </NavLink>
+              :  
               <NavLink to={menu.link} className="nav-links-menu" key={menu.title + idx}>
                 {menu.title}
               </NavLink>
             );
           })}
+          {isLogin.status && <div className="nav_userLogin">
+            <div onClick={()=>{setIsOpenProfile(!isOpenProfile)}} className="nav_userLogin_details">
+              <div className="nav_userImg">{isLogin?.name.charAt(0)}</div>
+              <div className="nav_userName">{isLogin?.name}<IoMdArrowDropdown /></div>
+            </div>
+            {isOpenProfile && <div className="nav_userLogin_option">
+              <ul>
+                <li>Your Profile</li>
+                <li onClick={handlelogout}>Logout</li>
+              </ul>
+            </div>}
+          </div>}
         </div>
         <FontAwesomeIcon className="nav-mob-bar" onClick={()=>{setIsMobileNavOpen(!isMobileNavOpen)}} size="lg" icon={faBars} />
       </motion.div>
@@ -74,11 +116,29 @@ function NavBar() {
 
           {NavMenu.map((menu, idx) => {
             return (
+              isLogin.status?
+                menu.title !== 'Login/Sign Up' &&
+              <NavLink to={menu.link} className="nav-links-menu mob_menu" key={menu.title + idx}>
+                {menu.title}
+              </NavLink>
+              :  
               <NavLink to={menu.link} className="nav-links-menu mob_menu" key={menu.title + idx}>
                 {menu.title}
               </NavLink>
             );
           })}
+          {isLogin.status && <div className="nav_userLogin">
+            <div onClick={()=>{setIsOpenProfile(!isOpenProfile)}} className="nav_userLogin_details">
+              <div className="nav_userImg">{isLogin?.name.charAt(0)}</div>
+              <div className="nav_userName">{isLogin?.name}<IoMdArrowDropdown /></div>
+            </div>
+            {isOpenProfile && <div className="nav_userLogin_option">
+              <ul>
+                <li>Your Profile</li>
+                <li onClick={handlelogout}>Logout</li>
+              </ul>
+            </div>}
+          </div>}
       </motion.div>
 
     </nav>
